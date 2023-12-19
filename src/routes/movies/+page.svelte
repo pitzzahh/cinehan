a31e823356f571615df3f3586f6fd3bc8c65388f
pitzzahh <pitzzahh@gmail.com>
2023-12-19 17:33:22 +0800

    🎨 Format structure

--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+	import type { PageData } from './$types';
+	export let data: PageData;
 </script>
