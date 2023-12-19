24e6d5f83165c07b2a3bd1c7c561e77a2f03eac0
pitzzahh <pitzzahh@gmail.com>
2023-12-19 17:31:21 +0800

    🥅 Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import { ExclamationTriangle } from 'radix-icons-svelte';
+	import * as Alert from '$lib/components/ui/alert';
+</script>
+<div class="container mt-4">
+    <Alert.Root variant="destructive">
+        <ExclamationTriangle class="h-4 w-4" />
+        <Alert.Title>Error {$page.status}</Alert.Title>
+        <Alert.Description>{$page.error?.message}</Alert.Description>
+    </Alert.Root>
+</div>
