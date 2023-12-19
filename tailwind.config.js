19534de5f72f753adf42498fc0cb1d7f1090b82e
pitzzahh <pitzzahh@gmail.com>
2023-12-19 21:08:58 +0800

    🔧 Add tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import { fontFamily } from "tailwindcss/defaultTheme";
+const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 const config = {
@@ -63,6 +64,33 @@ const config = {
 			}
 		}
 	},
+	plugins: [
+		plugin(function ({ addUtilities }) {
+			addUtilities({
+				'.scrollbar-hide': {
+					'-ms-overflow-style': 'none',
+					'scrollbar-width': 'none',
+					'&::-webkit-scrollbar': {
+						display: 'none'
+					}
+				}
+			}
+			)
+		}, ['responsive']),
+		plugin(function ({ addVariant, e, postcss }) {
+			addVariant('firefox', ({ container, separator }) => {
+				const isFirefoxRule = postcss.atRule({
+					name: '-moz-document',
+					params: 'url-prefix()'
+				});
+				isFirefoxRule.append(container.nodes);
+				container.append(isFirefoxRule);
+				isFirefoxRule.walkRules((rule) => {
+					rule.selector = `.${e(`firefox${separator}${rule.selector.slice(1)}`)}`;
+				});
+			});
+		})
+	]
 };
 
 export default config;
