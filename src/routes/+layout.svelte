db719192f793ba493162e637778f3c8e5c419ab2
pitzzahh <pitzzahh@gmail.com>
2023-12-19 17:31:56 +0800

    🎨 Reformat main route layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,11 @@
 <script>
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import '../app.pcss';
 	import { ModeWatcher } from 'mode-watcher';
-    import Header from '$lib/components/Header.svelte';
-	const links = [
-
-	];
+	import Header from '$lib/components/Header.svelte';
+	injectSpeedInsights();
 </script>
 
-<Header />
-
 <ModeWatcher />
+<Header />
 <slot />
