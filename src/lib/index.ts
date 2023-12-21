0cb3ba0d728d3af7fffd7b65e3398ce113909dd5
pitzzahh <pitzzahh@gmail.com>
2023-12-22 01:05:42 +0800

    ♻️ Removed tmdb from fetching info

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -9,49 +9,10 @@ export const store = writable({
 
 export let movieProvider = new MOVIES.FlixHQ()
 
-// export const fetchTrendingMovies = async (tmdbApiKey: string): Promise<IMovieInfo[]> => {
-//     try {
-//         console.log(`Fetching trending movies`);
-//         const trendingMovies = await movieProvider.fetchTrendingMovies()
-//         const trendingTitles = trendingMovies.map((e: IMovieResult) => {
-//             return e.id
-//         })
-//         console.log(`TRENDING MOVIES: ${JSON.stringify(trendingTitles)}`);
-
-//         const tmdb = new META.TMDB();
-//         const moviePromises: Promise<IMovieInfo | null>[] = trendingMovies.map(async (trendingMovie: IMovieResult) => {
-//             const searchResult = await tmdb.search(trendingMovie.title.toString(), 1) as ISearch<IMovieResult>;
-//             const result = searchResult.results[0];
-//             return result ? tmdb
-//                 .fetchMediaInfo(result.id, 'Movie')
-//                 .then((movie) => movie as IMovieInfo)
-//                 .catch(() => null) : null
-//         });
-//         return (await Promise.all(moviePromises)).filter((movie) => movie !== null) as IMovieInfo[];
-//     } catch (err: any) {
-//         console.error('Error fetching trending movies:', err);
-//         error(err.statusCode, 'Failed to fetch trending movies. Please try again later.');
-//     }
-// }
-
 export const fetchTrendingMovies = async (): Promise<IMovieInfo[]> => {
     try {
         console.log(`Fetching trending movies`);
-        const FlixHQTrendingMovies = await new MOVIES.FlixHQ().fetchTrendingMovies();
-        const trendingTitles = FlixHQTrendingMovies.map((e) => {
-            return e.title
-        })
-        console.log(`TRENDING MOVIES: ${JSON.stringify(trendingTitles)}`);
-
-        const tmdb = new META.TMDB();
-        const moviePromises: Promise<IMovieInfo | null>[] = FlixHQTrendingMovies.map(async (trendingMovie: IMovieResult) => {
-            const searchResult = await tmdb.search(trendingMovie.title.toString(), 1) as ISearch<IMovieResult>;
-            const result = searchResult.results[0];
-            return result ? tmdb
-                .fetchMediaInfo(result.id, 'Movie')
-                .then((movie) => movie as IMovieInfo)
-                .catch(() => null) : null
-        });
+        const moviePromises: Promise<IMovieInfo | null>[] = (await movieProvider.fetchTrendingMovies()).map(async (trendingMovie: IMovieResult) => await movieProvider.fetchMediaInfo(trendingMovie.id));
         return (await Promise.all(moviePromises)).filter((movie) => movie !== null) as IMovieInfo[];
     } catch (err: any) {
         console.error('Error fetching trending movies:', err);
@@ -66,4 +27,8 @@ export const formatRuntime = (runtime: number | undefined) => {
     const hours = Math.floor(runtime / 60);
     const minutes = runtime % 60;
     return `${hours}h ${minutes}m`;
+};
+
+export const formatDate = (dateString: string | undefined) => {
+    return dateString ? new Date(dateString).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }) : dateString;
 };
