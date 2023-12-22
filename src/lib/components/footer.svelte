15045a78a1620468a903c48c0ef3908c068b96ff
Peter John Arao <pitzzahh@gmail.com>
2023-12-22 13:21:37 +0800

    ✨ Add temporary footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,4 @@
+<footer class="place-items-center grid grid-rows-2 bg-primary-50 my-4">
+	<span class="text-md font-bold">All rights reserved</span>
+	<span>Contents are not saved on our servers</span>
+</footer>
