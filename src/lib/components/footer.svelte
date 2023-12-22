744ef3dd78668477de13b74bff2bcc803e6b13f9
Peter John Arao <pitzzahh@gmail.com>
2023-12-22 19:20:20 +0800

    🎨 Format footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,4 +1,6 @@
-<footer class="light-theme text-primary-foreground dark:text-secondary-foreground grid grid-rows-2 place-items-center p-4">
+<footer
+	class="light-theme grid grid-rows-2 place-items-center p-4 text-primary-foreground dark:text-secondary-foreground"
+>
 	<span class="text-md font-bold">Notice to viewers</span>
 	<span class="text-center text-sm"
 		>Movies & Tv Shows sourced from third-party providers — We don't store any content in this
