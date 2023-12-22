c79ec1240c3ace87244421a2331f3476513cdbaa
Peter John Arao <pitzzahh@gmail.com>
2023-12-22 14:15:11 +0800

    💄 Update footer style

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,4 +1,4 @@
 <footer class="place-items-center grid grid-rows-2 bg-primary-50 my-4">
-	<span class="text-md font-bold">All rights reserved</span>
-	<span>Contents are not saved on our servers</span>
+	<span class="text-md font-bold ">All rights reserved</span>
+	<span class="font-thin text-sm">Contents are not saved on our servers</span>
 </footer>
