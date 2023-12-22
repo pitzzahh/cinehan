c88f09780d58f737e2fca69cf4686df3383e934a
Peter John Arao <pitzzahh@gmail.com>
2023-12-22 10:42:38 +0800

    ✨ Add page progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,11 @@
 	import '../app.pcss';
 	import { ModeWatcher } from 'mode-watcher';
 	import Header from '$lib/components/Header.svelte';
+	import PageProgress from '$lib/components/page-progress.svelte';
 	injectSpeedInsights();
 </script>
 
+<PageProgress />
 <ModeWatcher />
 <Header />
 <slot />
