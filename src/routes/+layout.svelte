95fc917021f233d0a86fea23a931e0d1b6d719cd
Peter John Arao <pitzzahh@gmail.com>
2023-12-22 20:01:47 +0800

    🎨 Fix layout footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,22 @@
 <script>
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-	import '../app.pcss';
+	import '../app.pcss'; // Assuming this file contains your Tailwind CSS imports
 	import { ModeWatcher } from 'mode-watcher';
 	import Header from '$lib/components/header.svelte';
 	import Footer from '$lib/components/footer.svelte';
 	import PageProgress from '$lib/components/page-progress.svelte';
+
 	injectSpeedInsights();
 </script>
 
 <PageProgress />
 <ModeWatcher />
-<Header />
-<slot />
-<Footer/>
+<div class="flex min-h-screen flex-col">
+	<Header />
+
+	<main class="flex-grow">
+		<slot />
+	</main>
+
+	<Footer />
+</div>
