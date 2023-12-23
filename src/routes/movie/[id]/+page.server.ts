b2af5e40ee9396d9431624f99a40134e15d5da7b
Peter John Arao <pitzzahh@gmail.com>
2023-12-23 12:22:50 +0800

    🥅 Add error handling to movie page server load

--- src/routes/movie/[id]/+page.server.ts
+++ src/routes/movie/[id]/+page.server.ts
@@ -1,15 +1,21 @@
 import { movieProvider } from '$lib';
 import type { IMovieEpisode, IMovieInfo } from '@consumet/extensions';
 import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
 export const load = (async ({ params }) => {
-    const movieId = `movie/${params.id}`
-    console.log(`Params: ${JSON.stringify(movieId)}`)
-    const result: IMovieInfo = await movieProvider.fetchMediaInfo(movieId)
-    const episodes = result?.episodes as IMovieEpisode[]
-    return {
-        movieInfo: result,
-        episodes,
-        sources: (await movieProvider.fetchEpisodeSources(episodes[0].id, movieId))
-    };
+    try {
+        const movieId = `movie/${params.id}`
+        console.log(`Params: ${JSON.stringify(movieId)}`)
+        const result: IMovieInfo = await movieProvider.fetchMediaInfo(movieId)
+        const episodes = result?.episodes as IMovieEpisode[]
+        return {
+            movieInfo: result,
+            episodes,
+            sources: (await movieProvider.fetchEpisodeSources(episodes[0].id, movieId))
+        };
+    } catch (err: any) {
+        console.error('Error fetching movie info:', err);
+        error(err.statusCode, 'Failed to fetch movie info. Please try again later.');
+    }
 }) satisfies PageServerLoad;
