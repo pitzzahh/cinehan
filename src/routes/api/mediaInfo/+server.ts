9bd1e715eb3d7c9a2c0407f68e303c853d13adaf
Peter John Arao <pitzzahh@gmail.com>
2023-12-24 21:57:09 +0800

    ✨ Add mediaInfo server route

---
+++ src/routes/api/mediaInfo/+server.ts
@@ -0,0 +1,21 @@
+import { getStreamingServer, movieProvider } from '$lib';
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const POST: RequestHandler = async ({ request }) => {
+    const { episodeId, mediaId, server } = await request.json()
+    console.log(`Info params: ${JSON.stringify(episodeId, mediaId)}`)
+    const result = await movieProvider.fetchEpisodeSources(episodeId, mediaId, getStreamingServer(server.toLowerCase()));
+    const qualities = result.sources.map((source) => ({
+        name: source.quality,
+        value: source.url,
+        default: source.quality === 'auto'
+    }));
+    return new Response(JSON.stringify(qualities), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET'
+        }
+    });
+};
