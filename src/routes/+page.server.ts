e6aa31b5284bc691707247225e0c5ca98c005acb
Peter John Arao <pitzzahh@gmail.com>
2023-12-24 23:51:00 +0800

    🔥 Remove prerender flag

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,6 @@
 import type { PageServerLoad } from "./$types";
 import { fetchTrendingMovies } from "$lib";
 
-export const prerender = true
-
 export const load: PageServerLoad = async () => {
     return {
         streamed: {
